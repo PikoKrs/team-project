afa94fba9b5b3b05193fec979ff00adf419e1f0d
Artem Maksimov <artmaks2007@gmail.com>
2026-09-24 13:37:14 +0700

    feat: функции варианта 29 (закон Ома): maksimov.h, maksimov.cpp

---
+++ maksimov.h
@@ -0,0 +1,9 @@
+#ifndef MAKSIMOV_H
+#define MAKSIMOV_H
+
+// Функции варианта 29 (Максимов А. М., группа ПИ-52)
+double current(double U, double R);
+double resistance(double U, double I);
+double electricPower(double U, double I);
+
+#endif // MAKSIMOV_H
